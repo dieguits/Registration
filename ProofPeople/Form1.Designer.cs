740453c4b7d63e901e89d7c99f28a1031904266a
Diego Perez <dapohp@gmail.com>
2019-04-06 15:23:03 -0400

    initial commit

---
+++ ProofPeople/Form1.Designer.cs
@@ -0,0 +1,364 @@
+﻿namespace ProofPeople
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_nuevo = new System.Windows.Forms.Button();
+            this.dgvFornec = new System.Windows.Forms.DataGridView();
+            this.txtFirstName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtLastName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.cbxCasado = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.cbxChilLess16 = new System.Windows.Forms.ComboBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.cbxChilGre16 = new System.Windows.Forms.ComboBox();
+            this.nombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.apellido = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Casado = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.celular = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.email = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.menores_16 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.mayores_16 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvFornec)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_nuevo
+            // 
+            this.btn_nuevo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.btn_nuevo.FlatAppearance.BorderSize = 0;
+            this.btn_nuevo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_nuevo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_nuevo.ForeColor = System.Drawing.SystemColors.Control;
+            this.btn_nuevo.Location = new System.Drawing.Point(528, 443);
+            this.btn_nuevo.Name = "btn_nuevo";
+            this.btn_nuevo.Size = new System.Drawing.Size(106, 44);
+            this.btn_nuevo.TabIndex = 1;
+            this.btn_nuevo.Text = "Cancelar";
+            this.btn_nuevo.UseVisualStyleBackColor = false;
+            this.btn_nuevo.Click += new System.EventHandler(this.btn_nuevo_Click);
+            // 
+            // dgvFornec
+            // 
+            this.dgvFornec.AllowUserToAddRows = false;
+            this.dgvFornec.AllowUserToDeleteRows = false;
+            this.dgvFornec.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvFornec.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.nombre,
+            this.apellido,
+            this.Casado,
+            this.celular,
+            this.email,
+            this.menores_16,
+            this.mayores_16});
+            this.dgvFornec.Location = new System.Drawing.Point(50, 271);
+            this.dgvFornec.Name = "dgvFornec";
+            this.dgvFornec.ReadOnly = true;
+            this.dgvFornec.Size = new System.Drawing.Size(821, 150);
+            this.dgvFornec.TabIndex = 2;
+            // 
+            // txtFirstName
+            // 
+            this.txtFirstName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFirstName.Location = new System.Drawing.Point(182, 43);
+            this.txtFirstName.Name = "txtFirstName";
+            this.txtFirstName.Size = new System.Drawing.Size(181, 26);
+            this.txtFirstName.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(55, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "First Name:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(55, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Last Name:";
+            // 
+            // txtLastName
+            // 
+            this.txtLastName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtLastName.Location = new System.Drawing.Point(182, 85);
+            this.txtLastName.Name = "txtLastName";
+            this.txtLastName.Size = new System.Drawing.Size(181, 26);
+            this.txtLastName.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(494, 43);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(125, 20);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Phone Number:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(543, 85);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 20);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Email:";
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPhone.Location = new System.Drawing.Point(625, 43);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(228, 26);
+            this.txtPhone.TabIndex = 9;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEmail.Location = new System.Drawing.Point(625, 85);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(228, 26);
+            this.txtEmail.TabIndex = 10;
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.btnGuardar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnGuardar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGuardar.ForeColor = System.Drawing.SystemColors.Control;
+            this.btnGuardar.Location = new System.Drawing.Point(401, 443);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(106, 44);
+            this.btnGuardar.TabIndex = 11;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = false;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // cbxCasado
+            // 
+            this.cbxCasado.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbxCasado.FormattingEnabled = true;
+            this.cbxCasado.Items.AddRange(new object[] {
+            "Yes",
+            "No"});
+            this.cbxCasado.Location = new System.Drawing.Point(182, 130);
+            this.cbxCasado.Name = "cbxCasado";
+            this.cbxCasado.Size = new System.Drawing.Size(181, 28);
+            this.cbxCasado.TabIndex = 12;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(80, 131);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(71, 20);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Married:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(424, 175);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(252, 20);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Number of children less than 16:";
+            // 
+            // cbxChilLess16
+            // 
+            this.cbxChilLess16.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbxChilLess16.FormattingEnabled = true;
+            this.cbxChilLess16.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7"});
+            this.cbxChilLess16.Location = new System.Drawing.Point(688, 172);
+            this.cbxChilLess16.Name = "cbxChilLess16";
+            this.cbxChilLess16.Size = new System.Drawing.Size(165, 28);
+            this.cbxChilLess16.TabIndex = 15;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(402, 216);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(274, 20);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "Number of children greater than 16:";
+            // 
+            // cbxChilGre16
+            // 
+            this.cbxChilGre16.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbxChilGre16.FormattingEnabled = true;
+            this.cbxChilGre16.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7"});
+            this.cbxChilGre16.Location = new System.Drawing.Point(688, 213);
+            this.cbxChilGre16.Name = "cbxChilGre16";
+            this.cbxChilGre16.Size = new System.Drawing.Size(165, 28);
+            this.cbxChilGre16.TabIndex = 17;
+            // 
+            // nombre
+            // 
+            this.nombre.DataPropertyName = "nombre";
+            this.nombre.HeaderText = "First Name";
+            this.nombre.Name = "nombre";
+            this.nombre.ReadOnly = true;
+            this.nombre.Width = 110;
+            // 
+            // apellido
+            // 
+            this.apellido.DataPropertyName = "apellido";
+            this.apellido.HeaderText = "Last Name";
+            this.apellido.Name = "apellido";
+            this.apellido.ReadOnly = true;
+            this.apellido.Width = 110;
+            // 
+            // Casado
+            // 
+            this.Casado.DataPropertyName = "casado";
+            this.Casado.HeaderText = "Married";
+            this.Casado.Name = "Casado";
+            this.Casado.ReadOnly = true;
+            // 
+            // celular
+            // 
+            this.celular.DataPropertyName = "celular";
+            this.celular.HeaderText = "Cell Phone";
+            this.celular.Name = "celular";
+            this.celular.ReadOnly = true;
+            // 
+            // email
+            // 
+            this.email.DataPropertyName = "email";
+            this.email.HeaderText = "Email";
+            this.email.Name = "email";
+            this.email.ReadOnly = true;
+            // 
+            // menores_16
+            // 
+            this.menores_16.DataPropertyName = "menores_16";
+            this.menores_16.HeaderText = "Less than 16";
+            this.menores_16.Name = "menores_16";
+            this.menores_16.ReadOnly = true;
+            // 
+            // mayores_16
+            // 
+            this.mayores_16.DataPropertyName = "mayores_16";
+            this.mayores_16.HeaderText = "Greater than 16";
+            this.mayores_16.Name = "mayores_16";
+            this.mayores_16.ReadOnly = true;
+            this.mayores_16.Width = 110;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(902, 509);
+            this.Controls.Add(this.cbxChilGre16);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.cbxChilLess16);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.cbxCasado);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtPhone);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtLastName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtFirstName);
+            this.Controls.Add(this.dgvFornec);
+            this.Controls.Add(this.btn_nuevo);
+            this.Name = "Form1";
+            this.Text = "People Collection";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvFornec)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btn_nuevo;
+        private System.Windows.Forms.DataGridView dgvFornec;
+        private System.Windows.Forms.TextBox txtFirstName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtLastName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.ComboBox cbxCasado;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox cbxChilLess16;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ComboBox cbxChilGre16;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nombre;
+        private System.Windows.Forms.DataGridViewTextBoxColumn apellido;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Casado;
+        private System.Windows.Forms.DataGridViewTextBoxColumn celular;
+        private System.Windows.Forms.DataGridViewTextBoxColumn email;
+        private System.Windows.Forms.DataGridViewTextBoxColumn menores_16;
+        private System.Windows.Forms.DataGridViewTextBoxColumn mayores_16;
+    }
+}
+
---
+++ ProofPeople/Form1.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Odbc;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Globalization;
+
+namespace ProofPeople
+{
+    public partial class Form1 : Form
+    {
+        private OleDbConnection conn;
+        private OleDbCommand _oleCmd;
+        //private static String _Arquivo = @"C:\dados\Excel\Empregados.xlsx";
+        //private String connectionString = String.Format(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source={0};Extended Properties='Excel 12.0 Xml;HDR=YES;ReadOnly=False;C:\temp\empleados.xlsx'");
+        //private String sql;
+
+        OleDbConnection objConn;
+        String connection = String.Format(@"Provider=Microsoft.ACE.OLEDB.12.0; Data Source=C:\temp\empleados.xlsx;Extended Properties='Excel 12.0 Xml;HDR=YES;'");
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnIniciar_Click(object sender, EventArgs e)
+        {
+
+            //private String archivo = @"C:\temp\empleados.xlsx";
+            //private String connectionString = String.Format(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source={0};Extended Properties='Excel 12.0 Xml;HDR=YES;ReadOnly=False';"+ archivo);
+            //string connectionString = string.Format("Provider=Microsoft.Jet.OLEDB.4.0; data source={0}; Extended Properties=\"Excel 8.0;HDR=No;IMEX=1\";");
+            //"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\temp\empleados.xls"
+            // Provider=Microsoft.ACE.OLEDB.12.0;Data Source=c:\myFolder\myExcel2007file.xlsx;
+            //string connectionString = string.Format('@"Driver={Microsoft Excel Driver (*.xlsx)};Data Source=c:\temp\empleados.xlsx;"');
+            //@"Driver={Microsoft Excel Driver (*.xls)};DBQ=C:\temp\empleados.xls";
+
+            //OdbcConnection conn = new OdbcConnection(connectionString);
+            //conn = new OleDbConnection(connectionString);
+            //sql = "Select * FROM [empleadostbl$]";
+
+            conn.Open();
+            //OdbcDataAdapter da = new OdbcDataAdapter(sql, conn);
+
+            DataSet ds = new DataSet();
+
+            //da.Fill(ds, "Empleados");
+        }
+
+        private void btn_nuevo_Click(object sender, EventArgs e)
+        {
+            limpiarFormulario();
+        }
+
+        private void limpiarFormulario()
+        {
+            try
+            {
+                txtFirstName.Text = "";
+                txtLastName.Text = "";
+                txtEmail.Text = "";
+                txtPhone.Text = "";
+                cbxCasado.SelectedIndex = -1;
+                cbxChilLess16.SelectedIndex = -1;
+                cbxChilGre16.SelectedIndex = -1;
+            }catch(Exception ex)
+            {
+                MessageBox.Show("Cannot clean the form , Error:" + ex.ToString());
+            }
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            if (validarCampos())
+            {
+                if (guardarRegistro())
+                {
+                    limpiarFormulario();
+                    llenarFormulario();
+                    MessageBox.Show("Register saved.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("You must enter the data in the correct fields.", "Validation");
+            }
+        }
+
+        private bool guardarRegistro()
+        {
+            bool flag = true;
+            string sql = null;
+
+            try
+            {
+                OleDbCommand objCmdInsert = new OleDbCommand();
+                objConn = new OleDbConnection(connection);
+
+                string casado = "";
+                string menores = null;
+                string mayores = null;
+
+                if (cbxCasado.SelectedIndex != -1)
+                {
+                    casado = cbxCasado.SelectedItem.ToString();
+                }
+                else
+                {
+                    casado = "null";
+                }
+
+                if (cbxChilLess16.SelectedIndex != -1)
+                {
+                    menores = cbxChilLess16.SelectedItem.ToString();
+                }
+                else
+                {
+                    menores = "null";
+                }
+
+                if(cbxChilGre16.SelectedIndex != -1)
+                {
+                    mayores = cbxChilGre16.SelectedItem.ToString();
+                }else
+                {
+                    mayores = "null";
+                }
+
+
+                objConn.Open();
+                objCmdInsert.Connection = objConn;
+                sql = "INSERT into [empleadostbl$] (nombre, " +
+                                                   "apellido, " +
+                                                   "casado," +
+                                                   "celular," +
+                                                   "email," +
+                                                   "menores_16," +
+                                                   "mayores_16) " +
+                                           "values('" + CultureInfo.CurrentCulture.TextInfo.ToTitleCase(txtFirstName.Text) + "', " +
+                                                   "'" + CultureInfo.CurrentCulture.TextInfo.ToTitleCase(txtLastName.Text) + "'," +
+                                                   "" + casado + ", " +
+                                                   "'" + txtPhone.Text + "'," +
+                                                   "'" + txtEmail.Text + "'," +
+                                                   "" + menores + "," +
+                                                   "" + mayores + " )";
+
+                objCmdInsert.CommandText = sql;
+                objCmdInsert.ExecuteNonQuery();
+                objConn.Close();                
+
+            }catch(Exception e)
+            {
+                flag = false;
+                MessageBox.Show("Cannot insert new person, " + e.ToString(), "Error");
+            }
+
+            return flag;
+        }
+
+        /**
+         * Method to validate the empty fields. 
+         * 
+         * @author Diego.Perez.
+         * @date 01/04/2019.
+         **/
+        private bool validarCampos()
+        {
+            bool flag = true;
+
+            if(txtFirstName.Text == null || txtFirstName.Text == "")
+            {
+                flag = false;
+            }
+
+            if(txtLastName.Text == null || txtLastName.Text == "")
+            {
+                flag = false;
+            }
+
+            if(txtEmail.Text == null || txtEmail.Text == "")
+            {
+                flag = false;
+            }
+
+            if(txtPhone.Text == null || txtPhone.Text == "")
+            {
+                flag = false;
+            }
+
+            return flag;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            llenarFormulario();
+        }
+
+        private void llenarFormulario()
+        {
+            objConn = new OleDbConnection(connection);
+            //nombre	apellido					
+            OleDbCommand objCmdSelect = new OleDbCommand("SELECT nombre, " +
+                                                                "apellido, " +
+                                                                "casado, " +
+                                                                "celular, " +
+                                                                "email, " +
+                                                                "menores_16, " +
+                                                                "mayores_16 " +
+                                                          "FROM [empleadostbl$]", objConn);
+            //objCmdSelect.CommandText = "";
+            objConn.Open();
+
+            objCmdSelect.CommandType = CommandType.Text;
+            OleDbDataAdapter da = new OleDbDataAdapter(objCmdSelect);
+            DataTable clientes = new DataTable();
+            da.Fill(clientes); //name of the datagridviewer
+            dgvFornec.DataSource = clientes;
+        }
+    }
+}
